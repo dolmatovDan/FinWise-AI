4549088de0700d777217539f204cb46b65654f37
Anton Kapralov <toxakap2017@gmail.com>
2025-10-19 00:03:37 +0300

    Linear and pie chart in the first approximation
    diff --git c/mobile/app/src/main/java/com/spbsu_team7/finwise/core/repository/TestRepository.kt i/mobile/app/src/main/java/com/spbsu_team7/finwise/core/repository/TestRepository.kt
    index dcace52..298e0a9 100644
    --- c/mobile/app/src/main/java/com/spbsu_team7/finwise/core/repository/TestRepository.kt
    +++ i/mobile/app/src/main/java/com/spbsu_team7/finwise/core/repository/TestRepository.kt
    @@ -11,6 +11,7 @@ import com.spbsu_team7.finwise.core.model.Category
     import com.spbsu_team7.finwise.core.model.Transaction
     import java.time.Instant
     import com.spbsu_team7.finwise.core.model.Status
    +import java.time.ZoneId
     import kotlin.math.absoluteValue
    
     class TestRepository : Repository {
    @@ -45,31 +46,60 @@ class TestRepository : Repository {
             Advice("Меньше надо есть!")
         )
    
    -    override  fun getTransactions(): List<Transaction> =
    -        {Log.d("print", transactionList.map { it.name }.toString())
    -            transactionList
    -        }()
    +    override suspend fun getTransactions(): List<Transaction>
    +        {
    +            Log.d("print", transactionList.map { it.name }.toString())
    +            return transactionList
    +        }
    
    -    override  fun getStatus(): Status = {
    +    override suspend fun getStatus(): Status {
             Log.d("status", "status")
    -        Status(
    +        return Status(
                 transactionList.filter { it.amount >= 0 }.sumOf { it.amount },
                 transactionList.filter { it.amount < 0 }.sumOf { it.amount.absoluteValue },
                 transactionList.sumOf { it.amount }
    -        )}()
    +        )
    +    }
    
    -    override  fun getCategories(): List<Category> = categoryList
    +    override suspend fun getCategories(): List<Category> = categoryList
    
    -    override  fun getAdvices(): List<Advice> = adviceList
    +    override suspend fun getAdvices(): List<Advice> = adviceList
    
    -    override fun sendTransaction(transaction: Transaction) {
    +    override suspend fun sendTransaction(transaction: Transaction) {
             Log.d("add", "add")
             transactionList.add(transaction)
         }
    
    -    override  fun sendCategory(category: Category) {
    +    override suspend fun sendCategory(category: Category) {
             categoryList.add(category)
         }
    +
    +    override suspend fun getLastMonthsTransaction(months: Int): Pair<List<Int>, List<Int>> {
    +        val allTransactions = getTransactions().groupBy {
    +            it.date.atZone(ZoneId.systemDefault()).year * 12 +
    +                    it.date.atZone(ZoneId.systemDefault()).monthValue
    +        }
    +            .mapValues { it.value.map { it.amount } }
    +        val currentTime = Instant.now().atZone(ZoneId.systemDefault())
    +        val currentMonth = currentTime.year * 12 + currentTime.monthValue
    +        val periodTransactions = allTransactions.filter { currentMonth - it.key in 0..months }
    +        val incomeResults = periodTransactions.mapValues {
    +            it.value.sumOf { maxOf(it, 0) }
    +        }
    +        val expenseResults = periodTransactions.mapValues {
    +            it.value.sumOf { -minOf(it, 0) }
    +        }
    +        return Pair(
    +            List(months) { incomeResults.getOrDefault(currentMonth + it, 0) },
    +            List(months) { expenseResults.getOrDefault(currentMonth + it, 0) }
    +        )
    +    }
    +
    +    override suspend fun getCategoriesExpense(): Map<Category, Int> {
    +        return getTransactions().filter { it.amount < 0 }.groupBy {
    +            it.category
    +        }.mapValues { it.value.sumOf { -it.amount } }
    +    }
     }

--- mobile/app/src/main/java/com/spbsu_team7/finwise/core/repository/TestRepository.kt
+++ mobile/app/src/main/java/com/spbsu_team7/finwise/core/repository/TestRepository.kt
@@ -11,6 +11,7 @@ import com.spbsu_team7.finwise.core.model.Category
 import com.spbsu_team7.finwise.core.model.Transaction
 import java.time.Instant
 import com.spbsu_team7.finwise.core.model.Status
+import java.time.ZoneId
 import kotlin.math.absoluteValue
 
 class TestRepository : Repository {
@@ -45,30 +46,59 @@ class TestRepository : Repository {
         Advice("Меньше надо есть!")
     )
 
-    override  fun getTransactions(): List<Transaction> =
-        {Log.d("print", transactionList.map { it.name }.toString())
-            transactionList
-        }()
+    override suspend fun getTransactions(): List<Transaction>
+        {
+            Log.d("print", transactionList.map { it.name }.toString())
+            return transactionList
+        }
 
-    override  fun getStatus(): Status = {
+    override suspend fun getStatus(): Status {
         Log.d("status", "status")
-        Status(
+        return Status(
             transactionList.filter { it.amount >= 0 }.sumOf { it.amount },
             transactionList.filter { it.amount < 0 }.sumOf { it.amount.absoluteValue },
             transactionList.sumOf { it.amount }
-        )}()
+        )
+    }
 
-    override  fun getCategories(): List<Category> = categoryList
+    override suspend fun getCategories(): List<Category> = categoryList
 
-    override  fun getAdvices(): List<Advice> = adviceList
+    override suspend fun getAdvices(): List<Advice> = adviceList
 
-    override fun sendTransaction(transaction: Transaction) {
+    override suspend fun sendTransaction(transaction: Transaction) {
         Log.d("add", "add")
         transactionList.add(transaction)
     }
 
-    override  fun sendCategory(category: Category) {
+    override suspend fun sendCategory(category: Category) {
         categoryList.add(category)
+    }
+
+    override suspend fun getLastMonthsTransaction(months: Int): Pair<List<Int>, List<Int>> {
+        val allTransactions = getTransactions().groupBy {
+            it.date.atZone(ZoneId.systemDefault()).year * 12 +
+                    it.date.atZone(ZoneId.systemDefault()).monthValue
+        }
+            .mapValues { it.value.map { it.amount } }
+        val currentTime = Instant.now().atZone(ZoneId.systemDefault())
+        val currentMonth = currentTime.year * 12 + currentTime.monthValue
+        val periodTransactions = allTransactions.filter { currentMonth - it.key in 0..months }
+        val incomeResults = periodTransactions.mapValues {
+            it.value.sumOf { maxOf(it, 0) }
+        }
+        val expenseResults = periodTransactions.mapValues {
+            it.value.sumOf { -minOf(it, 0) }
+        }
+        return Pair(
+            List(months) { incomeResults.getOrDefault(currentMonth + it, 0) },
+            List(months) { expenseResults.getOrDefault(currentMonth + it, 0) }
+        )
+    }
+
+    override suspend fun getCategoriesExpense(): Map<Category, Int> {
+        return getTransactions().filter { it.amount < 0 }.groupBy {
+            it.category
+        }.mapValues { it.value.sumOf { -it.amount } }
     }
 }
 
